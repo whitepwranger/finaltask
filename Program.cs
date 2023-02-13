b692b3d794b028de22950c3e482bcda9804a4510
Your Mom <skibski.eugene@gmail.com>
2023-02-13 20:34:43 +0300

    Created programm

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿// add branch main
+string[] arr = {"12323","ta", "cat", "1", "Hellow"};
+string[] arr_result = new string[arr.Length]; 
+string result = "";
+for(int i = 0, j = 0; i < arr.Length; ++i){
+    if(arr[i].Length < 4){
+        arr_result[j] = arr[i];
+        Console.Write(arr[i] + " ");
+    }
+}
+Console.WriteLine(result);
